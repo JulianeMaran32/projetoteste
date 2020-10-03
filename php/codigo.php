d934470971004e01c2fd457c349bc29d33e01c0b
JULIANE MARAN <julianemaran@gmail.com>
2020-10-03 11:59:28 -0300

    todos os arquivos

---
+++ php/codigo.php
@@ -0,0 +1,28 @@
+<?php
+	// trecho do código php
+
+	$num1 = 8;
+	$num2 = 4;
+
+	echo "A soma entre $num1 e $num2 é = " . ($num1 + $num2);
+	echo "<br />";
+	
+	echo "A subtração entre $num1 e $num2 é = " . ($num1 - $num2);
+	echo "<br />";
+	
+	echo "A multiplicação entre $num1 e $num2 é = " . ($num1 * $num2);
+	echo "<br />";
+
+	echo "A divisão entre $num1 e $num2 é = " . ($num1 / $num2);
+	echo "<br />";
+
+	echo "O módulo entre $num1 e $num2 é = " . ($num1 % $num2);
+	echo "<br />";
+
+	// =
+	// +, -, *, /, %
+	
+	// $x = 10;
+	// $x += 5;
+	// echo $x;
+?>
